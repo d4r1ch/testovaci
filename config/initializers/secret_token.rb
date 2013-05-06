55d3240202ebd84d4f35fa0d1fa74a92f720b209
d4r1ch <d4r1ch@gmail.com>
2013-05-06 22:00:48 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pokusny::Application.config.secret_token = '55d343bb635157dc7907da23357d97ab4ce5583a845c53075403f92884ab6e9cb8b1fa8e474130748265d25785303d0c675eb9e4ac534370ba33ff59529df87d'
